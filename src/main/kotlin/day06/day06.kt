a2e78aa891f7705be9a11696f27ece9649211990
Humberd <humberd2@gmail.com>
2020-12-06 11:52:48 +0100

    Day 06 completed part 2

--- src/main/kotlin/day06/day06.kt
+++ src/main/kotlin/day06/day06.kt
@@ -4,7 +4,7 @@ fun main() {
     val text = getResourceAsText("data/day06.txt")
     val groups = text.split("\n\n")
 
-    val result = getResultPart1(groups)
+    val result = getResultPart2(groups)
 
     printResult(result)
 }
@@ -19,5 +19,42 @@ private fun getResultPart1(groups: List<String>): Int {
             .forEach { groupSet.add(it) }
 
         groupSet.size
+    }.sum()
+}
+
+private fun getResultPart2(groups: List<String>): Int {
+    return groups.map { group ->
+        val people = group.split("\n").filter { it != "" }
+
+        /**
+         * There is aa 32 bit int for every letter
+         * Each bit in an int is a representation if a given person answered 'yes'
+         * 100 - person 1 and person 2 answered 'no', but person 3 answered 'yes'
+         * 1111 - all 4 people answered 'yes'
+         */
+        val letterBytes = Array(QUESTIONS_COUNT, { 0 })
+
+        people.forEachIndexed { personIndex, person ->
+            person.forEach { letter ->
+                val bitPosition = (letter - 97).toInt()
+                letterBytes[bitPosition] = letterBytes[bitPosition] or (1 shl personIndex)
+            }
+        }
+
+        print("Group: ")
+        letterBytes.forEach {
+            print("${Integer.toBinaryString(it)}, ")
+        }
+        println("")
+
+        val peopleCount = people.size
+        val requiredNumber = (Math.pow(2.0, peopleCount.toDouble()) - 1).toInt()
+        val foo = letterBytes.map {
+            if (it == requiredNumber) {
+                1
+            } else
+                0
+        }
+        foo.sum()
     }.sum()
 }
--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -1,3 +1,5 @@
+import kotlin.math.sqrt
+
 fun getResourceAsText(path: String): String = object {}.javaClass.getResource(path).readText()
 fun getResourceAsLines(path: String): List<String> = getResourceAsText(path).split("\n").filter { it.length > 0 }
 
@@ -27,3 +29,15 @@ fun Iterable<Boolean>.sum(): Int {
     return sum
 }
 
+fun Int.isSqrtAWholeNumber(): Boolean {
+    val result = sqrt(this.toDouble())
+    val intedResult = result.toInt()
+
+    return (intedResult * intedResult).toDouble() == result
+}
+
+fun Int.isPowerOf2(): Boolean {
+    val n = this
+    return if (n == 0) false else Math.ceil(Math.log(n.toDouble()) / Math.log(2.0)).toInt() ==
+            Math.floor(Math.log(n.toDouble()) / Math.log(2.0)).toInt()
+}
