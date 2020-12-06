109d7ce961807e03534ca18612f6751f4cc826ed
humberd <humberd2@gmail.com>
2020-12-06 01:08:54 +0100

    Day04 part 2 done

--- src/main/kotlin/day04/day04.kt
+++ src/main/kotlin/day04/day04.kt
@@ -12,7 +12,7 @@ fun main() {
         Batch(mapOf(*pairs.toTypedArray()))
     }
 
-    val result = batches.map { part1IsValid(it) }
+    val result = batches.map { part2IsValid(it) }
         .sum()
 
     println("$result valid out of ${batches.size} batches")
@@ -29,6 +29,21 @@ private fun part1IsValid(batch: Batch): Boolean {
     }
 }
 
+private fun part2IsValid(batch: Batch): Boolean {
+    return if (batch.entries.size == 8) {
+        batch.entries.values.all { it.isValid() }
+    } else if (batch.entries.size < 7) {
+        false
+    } else {
+        val hasCid = batch.entries.containsKey("cid")
+        if (hasCid) {
+            return false
+        }
+
+        batch.entries.values.all { it.isValid() }
+    }
+}
+
 private data class Batch(
     val entries: Map<String, BatchEntry>
 )
@@ -36,14 +51,65 @@ private data class Batch(
 private sealed class BatchEntry(
     val key: String
 ) {
-    data class BirthYear(val value: Int) : BatchEntry("byr")
-    data class IssueYear(val value: Int) : BatchEntry("iyr")
-    data class ExpirationYear(val value: Int) : BatchEntry("eyr")
-    data class Height(val value: Int, val unit: String?) : BatchEntry("hgt")
-    data class HairColor(val value: String) : BatchEntry("hcl")
-    data class EyeColor(val value: String) : BatchEntry("ecl")
-    data class PassportId(val value: String) : BatchEntry("pid")
-    data class CountryId(val value: String) : BatchEntry("cid")
+    data class BirthYear(val value: Int) : BatchEntry("byr") {
+        override fun isValid(): Boolean {
+            return value >= 1920 && value <= 2002
+        }
+    }
+
+    data class IssueYear(val value: Int) : BatchEntry("iyr") {
+        override fun isValid(): Boolean {
+            return value >= 2010 && value <= 2020
+        }
+    }
+
+    data class ExpirationYear(val value: Int) : BatchEntry("eyr") {
+        override fun isValid(): Boolean {
+            return value >= 2020 && value <= 2030
+        }
+    }
+
+    data class Height(val value: Int, val unit: String?) : BatchEntry("hgt") {
+        override fun isValid(): Boolean {
+            if (unit == null) {
+                return false
+            }
+            if (unit == "cm" && value >= 150 && value <= 193) {
+                return true
+            }
+            if (unit == "in" && value >= 59 && value <= 76) {
+                return true
+            }
+
+            return false
+        }
+    }
+
+    data class HairColor(val value: String) : BatchEntry("hcl") {
+        override fun isValid(): Boolean {
+            return value.matches("#[0-9a-f]{6}".toRegex())
+        }
+    }
+
+    data class EyeColor(val value: String) : BatchEntry("ecl") {
+        override fun isValid(): Boolean {
+            return value.matches("amb|blu|brn|gry|grn|hzl|oth".toRegex())
+        }
+    }
+
+    data class PassportId(val value: String) : BatchEntry("pid") {
+        override fun isValid(): Boolean {
+            return value.matches("[0-9]{9}".toRegex())
+        }
+    }
+
+    data class CountryId(val value: String) : BatchEntry("cid") {
+        override fun isValid(): Boolean {
+            return true
+        }
+    }
+
+    abstract fun isValid(): Boolean;
 
     companion object {
         fun generateEntry(raw: String): BatchEntry {
